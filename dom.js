cbecf27de1ecb000184710aaf723009e7c26f694
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2022-12-20 17:55:18 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,7 @@
+var edit=document.getElementsByClassName("list-group-item");
+edit[2].style.backgroundColor="green"; 
+// for changing the color
+// for making the bold
+for(var i=0;i<edit.length;i++){
+    edit[i].style.fontWeight="bold";
+}
