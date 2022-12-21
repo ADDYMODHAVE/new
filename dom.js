a9e4271c36ff268afe1aefbb9b87b67b1e79864b
ADDYMODHAVE <118976941+ADDYMODHAVE@users.noreply.github.com>
2022-12-21 13:35:06 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -5,3 +5,9 @@ edit[2].style.backgroundColor="green";
 for(var i=0;i<edit.length;i++){
     edit[i].style.fontWeight="bold";
 }
+var tag=document.getElementsByTagName("li");
+console.log(tag);
+for(var i=0;i<tag.length;i++){
+       tag[i].style.fontWeight="bold";
+     }
+tag[4].style.color="red";
